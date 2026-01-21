ca81b25a938deac42340fd3b4bf088a8ddd2ba6c
Qbstero30 <149096912+Qbstero30@users.noreply.github.com>
2026-01-21 22:57:35 +0100

    LevelTemplate+Tileset

--- platformer_2026-01-20_20-22-20/platformer/Scripts/dude.gd
+++ platformer_2026-01-20_20-22-20/platformer/Scripts/dude.gd
@@ -6,14 +6,16 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	
 	velocity += get_gravity() * delta
-
+	
 	if Input.is_action_just_released("ui_up") and velocity.y < 0:
 		velocity.y = JUMP_VELOCITY / 4
-		
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	elif Input.is_action_just_pressed("ui_up") and is_on_ceiling():
+		velocity.y = -JUMP_VELOCITY
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -23,3 +25,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func _on_anty_gravity_area_body_entered(body: CharacterBody2D) -> void:
+	rotation_degrees = 180
+
+func _on_anty_gravity_area_body_exited(body: CharacterBody2D) -> void:
+	rotation_degrees = 0
+	
