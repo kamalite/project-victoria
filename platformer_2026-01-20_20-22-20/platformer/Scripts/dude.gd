a3ae0a7445a32753301772bc05ab318d13df000e
Qbstero30 <149096912+Qbstero30@users.noreply.github.com>
2026-01-21 23:03:03 +0100

    poprawka

--- platformer_2026-01-20_20-22-20/platformer/Scripts/dude.gd
+++ platformer_2026-01-20_20-22-20/platformer/Scripts/dude.gd
@@ -16,8 +16,6 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_just_pressed("ui_up") and is_on_ceiling():
 		velocity.y = -JUMP_VELOCITY
 		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
