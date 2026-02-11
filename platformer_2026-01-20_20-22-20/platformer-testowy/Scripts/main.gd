eeb7ef6a0c5afaaa836f73cd9d31fe17fcfe2ad0
ArtyperX <ajzielonka04@gmail.com>
2026-02-11 14:34:05 +0100

    Jeden i pół poziomu

--- platformer_2026-01-20_20-22-20/platformer-testowy/Scripts/main.gd
+++ platformer_2026-01-20_20-22-20/platformer-testowy/Scripts/main.gd
@@ -4,15 +4,19 @@ var Lvl1 = preload("res://Scenes/Levels/level_template.tscn")
 var Lvl2 = preload("res://Scenes/Levels/level_template_2.tscn")
 var Lvl3 = preload("res://Scenes/Levels/level_template_3.tscn")
 var Lvl4 = preload("res://Scenes/Levels/level_template_4.tscn")
+var Lvl5 = preload("res://Scenes/Levels/level_template_5.tscn")
 var instance1 = Lvl1.instantiate()
 var instance2 = Lvl2.instantiate()
 var instance3 = Lvl3.instantiate()
 var instance4 = Lvl4.instantiate()
+var instance5 = Lvl5.instantiate()
 var SpawnRight = Vector2(768, 0)
 var SpawnDown = Vector2(0, 432)
 var SpawnLeft = Vector2(-768, 0)
+var SpawnUp = Vector2(0, -432)
 var BottomLeftEntranceRooms = [instance3, instance3]
 var TopLeftEntraneRooms = [instance4]
+var TopNorthEntranceRooms = [instance5]
 
 func _ready() -> void:
 	
@@ -25,6 +29,10 @@ func _ready() -> void:
 	add_child(ThirdRoom)
 	TopLeftEntraneRooms.erase(ThirdRoom)
 	ThirdRoom.global_position = SecondRoom.global_position + SpawnRight
+	var ForthRoom = TopNorthEntranceRooms[randi()% TopNorthEntranceRooms.size()]
+	add_child(ForthRoom)
+	TopNorthEntranceRooms.erase(ForthRoom)
+	ForthRoom.global_position = ThirdRoom.global_position + SpawnUp
 	
 func _process(_delta: float) -> void:
 	pass
